29480dbd03406d644e735a61fe2532819c6cabec
Ben Bixby <ben@earthaid.net>
2011-03-09 11:29:15 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Benbixby::Application.config.secret_token = '9c114d2448565e148dc3382173941d3277f36856a5a91e3c6cf5925086d2670163201591fb1103fccb91bda643b90fdb67ed8e8e2bbb6ed880dd2c6513c81a3b'
